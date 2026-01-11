a22565c9fc432836118a8f7497b60668edaffe3f
pranav <pranavarun19@gmail>
2026-01-11 12:06:06 +0530

    edited 12program

--- 12.py
+++ 12.py
@@ -1,8 +1,6 @@
 #Program 12
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
 import seaborn as sns 
+import matplotlib.pyplot as plt 
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import mean_squared_error, r2_score 
@@ -16,7 +14,7 @@ model = LinearRegression()
 model.fit(X_train, y_train) 
 y_pred = model.predict(X_test) 
 mse = mean_squared_error(y_test, y_pred) 
-r2 = r2_score(y_test, y_pred) 
+r2 = r2_score(y_test, y_pred)
 print(f'Mean Squared Error: {mse}') 
 print(f'R-squared: {r2}') 
 plt.figure(figsize=(10, 6)) 
